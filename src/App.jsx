44e05de4fb8ec8599f698a8337f0a414e50f3358
Mohd Wazid <mohd.wazid.db@gmail.com>
2026-01-17 15:53:40 +0530

    2nd

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,5 @@
 import React, { useEffect, useState } from 'react'
+import Child from "./Child";
 
 const App = () => {
   const [count, setcount] = useState(0);
@@ -12,6 +13,7 @@ const App = () => {
       <h1>my value is {count}</h1>
       <button onClick={() => setcount(count + 1)}>click me</button>
       <button onClick={() => setcount(count - 1)}>click me</button>
+      <Child name="Wazid Ansari" age="24"></Child>
     </div>
   )
 }
